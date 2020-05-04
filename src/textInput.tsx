98ffd3fa9f7c27e1df25ea092d6030a2e8872409
danhuszti <huszti.dani@gmail.com>
2020-05-04 15:44:00 +0200

    commit of unsaved changes in repo

--- src/textInput.tsx
+++ src/textInput.tsx
@@ -40,28 +40,3 @@ export class TextInput extends Component<TextInputOptions>{
             </div>);
     }
 }
-
-export interface TextInputAndButtonOptions extends TextInputOptions {
-    buttonContent?: string;
-    onClick?: (text: string) => boolean | void;
-}
-
-export class TextInputAndButton extends Component<TextInputAndButtonOptions>
-{
-    textInput = React.createRef<TextInput>();
-
-    onClick() {
-        if (this.props.onClick?.(this.textInput.current?.state.value ?? ""))
-            this.textInput.current?.setState({ value: "" });
-    }
-    render() {
-        return (
-            <div className="text-input-and-button">
-                <TextInput {...this.props} onEnter={() => this.onClick()} />
-                <button type="button" onClick={() => this.onClick()}>
-                    {this.props.buttonContent}
-                </button>
-            </div>
-        );
-    }
-}
